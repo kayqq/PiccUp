f2bc654829c7d8f59e23e5720dc41aa921f7c2bf
Kenny Quan <quan_kenny@yahoo.com>
2019-01-23 05:37:58 -0800

    pruned out redux-logger

--- PiccUp-Front/src/store.js
+++ PiccUp-Front/src/store.js
@@ -1,10 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
-// import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
-
-// const loggerMiddleware = createLogger();
 
 export const store = createStore(
     rootReducer,
